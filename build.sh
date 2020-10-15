d63e59e56d80488d5bb469a8c9f6874daa456589
Shaul Fridman <sfridman@synamedia.com>
2020-10-15 09:58:51 +0300

    no conan

--- build.sh
+++ build.sh
@@ -3,12 +3,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 pushd $SCRIPT_DIR
 
-if ! which conan; then
-    echo "Conan C++ package manager isn't installed. installing it now"
-    pip install conan
-    conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-    conan remote add outcome https://api.bintray.com/conan/ned14/Outcome
-fi
 if [[ "$*" = *wasm* ]]; then HOST_PLATFORM=EMSCRIPTEN; fi
 if [[ "$*" = *clean* ]]; then rm -rf build; fi
 if [ ! -d "build" ]; then mkdir build; fi
